b580a7313b48ae0e011a3ab929d6320bd2fddd7e
Rizkiramdani04 <96227780+Rizkiramdani04@users.noreply.github.com>
2023-05-12 18:27:15 +0700

    Add files via upload

---
+++ gamedev.js
@@ -0,0 +1,16 @@
+const navbarNav = document.querySelector('.navbar-nav');
+document.querySelector('#hamburger').
+onclick = () => {
+    navbarNav.classList.toggle('active');
+}
+
+//luar sidebar//
+
+const hamburger = document.querySelector('#hamburger');
+document.addEventListener('click', function(e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target))
+    {
+        navbarNav.classList.remove('active');
+
+    }
+});
